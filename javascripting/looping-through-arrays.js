5e0bd4b4039a5e143f4530e10ee0638c6b395574
Satnam Singh Toor <Satnamtoor97@gmail.com>
2021-12-29 12:44:01 -0500

    Initial commit with all my work so far

---
+++ javascripting/looping-through-arrays.js
@@ -0,0 +1,7 @@
+const pets = ['cat', 'dog', 'rat']
+for (let i = 0; i < pets.length; i++) {
+  const element = pets[i] = pets[i] + 's'
+;
+  
+}
+console.log(pets)
---
+++ javascripting/number-to-string.js
@@ -0,0 +1,4 @@
+var n = 128
+n = n.toString
+toString=128
+console.log(toString)
