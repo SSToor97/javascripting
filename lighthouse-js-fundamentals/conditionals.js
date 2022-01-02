3ba97ea5b23afd465b6c2f10d388ec2c59d6e6a7
Satnam Singh Toor <Satnamtoor97@gmail.com>
2022-01-02 12:19:31 -0500

    Use conditionals to tell people about the weather

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -1,4 +1,14 @@
-const temperature = -1;
+
+
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+const temperature = 12;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -9,11 +19,3 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
-
-const isCitizen = true;
-const age = 26;
-
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
-}
-
